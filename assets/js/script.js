af7889537511d79f5fc9e8467bf5fd0fac86f6c7
Joe Rivera <dvs_joe@hotmail.com>
2023-05-03 22:43:02 -0400

    added time and colorization

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,60 @@
 // in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button.  
-  $(".saveBtn").on("click",function(){
-    var userInput=$(this).siblings(".description").val().trim();
-  })
+
+
+
+
+  function updateHour() {
+
+    $(".container-lg").children().each(function(){
+      $(this).remove();
+    })
+$("#currentDay").text(    dayjs().format("dddd, MMM, YYYY"))
+    //what hour it currently is
+    //for each time block update it's color according to the hour
+    var today = new Date()
+    var currentHour = today.getHours()
+    console.log(currentHour)
+    for (var i = 9; i < 19; i++) {
+      var colorKey = ""
+      if (i < currentHour) {
+        colorKey = "past"
+      } else if (i === currentHour) {
+        colorKey = "present"
+      }
+      else {
+        colorKey = "future"
+      }
+      
+      var timeBlock = $("<div>").addClass("row time-block ").attr("id", `hour-${i}`);
+      var timeText;
+
+      if (i>12){
+        timeText=i-12
+      }
+      else {
+        timeText=i
+      }
+
+      var hourDiv = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(timeText);
+      var textInput = $("<textarea>").addClass("col-8 col-md-10 description " + colorKey).attr("rows", "3").val(localStorage.getItem(`hour-${i}`))
+      var button = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save").on("click", function () {
+        var userInput = $(this).siblings(".description").val().trim();
+        var hour = $(this).parent().attr("id");
+        localStorage.setItem(hour, userInput);
+    
+      });
+      var icon = $("<i>").addClass("fas fa-save").attr("aria-hidden", "true")
+      $(".container-lg").append(timeBlock.append(hourDiv, textInput, button.append(icon)))
+
+    }
   
+  }
+  updateHour()
+  setInterval(updateHour,5000)
+ 
+
   //This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
